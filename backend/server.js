12b68f0f2aedda564f1f1775e8b6bd8658fed066
yashgauniya <yashgauniya47@gmail.com>
2025-07-13 11:48:04 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/notes', noteRoutes);
 
 initSocket(server);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
